e82d012fc6f472cd1872a2d30f38019f4aa513c2
PituchaAleksander <pitucha.aleksander@gmail.com>
2021-04-09 20:55:36 +0200

    ControllerFunDatabase

--- app/app/src/main/java/com/app/remote_controller_app/DatabaseHelper.java
+++ app/app/src/main/java/com/app/remote_controller_app/DatabaseHelper.java
@@ -3,12 +3,14 @@ package com.app.remote_controller_app;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.app.remote_controller_app.Fragments.Controller;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class DatabaseHelper  extends OrmLiteSqliteOpenHelper {
     private static final String DATABASE_NAME = "remote_controller.db";
@@ -40,5 +42,37 @@ public class DatabaseHelper  extends OrmLiteSqliteOpenHelper {
 //        } catch (SQLException e) {
 //            throw new RuntimeException(e);
 //        }
+    }
+
+    public void addController(Controller c){
+        try {
+            controllerDao.create(c);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateNameController(Controller c){
+        try {
+            controllerDao.update(c);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void removeController(Controller c){
+        try {
+            controllerDao.delete(c);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Controller> listController(){
+        try {
+            return controllerDao.queryForAll();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
