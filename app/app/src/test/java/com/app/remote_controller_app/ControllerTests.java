a936efe47078af0629ef426bd865ac89bae517b2
Jan Starosta <63151352+janjanek@users.noreply.github.com>
2021-04-18 16:49:24 +0200

    Create ControllerTests.java
    Controller Testing

---
+++ app/app/src/test/java/com/app/remote_controller_app/ControllerTests.java
@@ -0,0 +1,30 @@
+package com.app.remote_controller_app;
+import com.app.remote_controller_app.components.Button;
+import com.app.remote_controller_app.components.Component;
+import com.app.remote_controller_app.fragments.ControllerMenu;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ControllerTests {
+    @Test
+    public void createController(){
+        Controller c = new Controller("name1", "12345");
+        c.addComponent(new Button(1,5,1.5f,1.8f,false));
+        assertEquals("Controller{name='name1', favoriteMAC='12345', listOfComponents=[Button{sizeX=1.0, sizeY=5.0, posX=1.5, posY=1.8, isLocal=false}]}", c.toString());
+    }
+
+
+    @Test
+    public void controllerCheckId(){
+        Controller c = new Controller("name1", "12345");
+        c.addComponent(new Button(1,5,1.5f,1.8f,false));
+        c.addComponent(new Button(5,15,6.5f,24,false));
+        assertEquals(0,c.getId());
+        c.setId(1);
+        assertEquals(1,c.getId());
+    }
+}
