ebb1182eb03c7a4e3230fcac6475cad000fae812
rydza <rydzakmistrz@gmail.com>
2021-04-19 08:38:54 +0200

    Add Controller Front-end

--- app/app/src/main/java/com/app/remote_controller_app/fragments/Opening.java
+++ app/app/src/main/java/com/app/remote_controller_app/fragments/Opening.java
@@ -1,5 +1,6 @@
 package com.app.remote_controller_app.fragments;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -9,81 +10,86 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.app.remote_controller_app.MainActivity;
 import com.app.remote_controller_app.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 
 public class Opening extends Fragment {
 
-    private ConstraintLayout mLayout;
-    private EditText textButtonName;
-    private FloatingActionButton buttonAddController;
+    ListView listView;
+    EditText textButtonName;
+    FloatingActionButton buttonAddController;
+    View dialogLayout;
+    ArrayAdapter<String> arrayAdapter;
+
+    ArrayList<String> arrayList = new ArrayList<>();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    // Avoid passing null as the view of root
+    @SuppressLint("InflateParams")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_opening, container, false);
 
-//        FloatingActionButton btnAddController = (FloatingActionButton) view.findViewById(R.id.button_addController);
-//        btnAddController.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-        mLayout = (ConstraintLayout) view.findViewById(R.id.__opening);
+        listView = (ListView) view.findViewById(R.id.list_view_layout);
+
 
         buttonAddController = (FloatingActionButton) view.findViewById(R.id.button_addController);
-        buttonAddController.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                View dialogLayout = getLayoutInflater().inflate(R.layout.dialog_add_controller_layout, null);
+        buttonAddController.setOnClickListener(listenerAddController);
 
-                builder.setView(dialogLayout);
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        textButtonName = dialogLayout.findViewById(R.id.dialog_add_controller_edit_name);
-                        mLayout.addView(createNewButton(textButtonName.getText().toString()));
-                    }
-                });
-                builder.setNegativeButton("Cancel", null);
+        arrayList.add("sample");
 
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
-        });
+        arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
 
+        listView.setOnItemClickListener(listenerGetController);
         return view;
     }
 
+    View.OnClickListener listenerAddController = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            dialogLayout = getLayoutInflater().inflate(R.layout.dialog_add_controller_layout, null);
 
-    private Button createNewButton(String text) {
-        LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        Button newButton = new Button(getContext());
+            builder.setView(dialogLayout);
+            builder.setPositiveButton("OK", listenerButtonCreation);
+            builder.setNegativeButton("Cancel", null);
 
-        newButton.setLayoutParams(lparams);
-        newButton.setText(text);
-        newButton.setTag(text);
-        newButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), (String) v.getTag(), Toast.LENGTH_SHORT).show();
-            }
-        });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    };
 
-        return newButton;
-    }
+    DialogInterface.OnClickListener listenerButtonCreation = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            textButtonName = dialogLayout.findViewById(R.id.dialog_add_controller_edit_name);
+            arrayList.add( String.valueOf(textButtonName.getText()) );
+        }
+    };
 
+    AdapterView.OnItemClickListener listenerGetController = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(getContext(), arrayList.get(position), Toast.LENGTH_SHORT).show();
+        }
+    };
 }
